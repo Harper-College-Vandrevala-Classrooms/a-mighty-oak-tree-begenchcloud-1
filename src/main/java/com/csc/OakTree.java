9f5411c7505c779fcce3eb3d1f73a5d905c78326
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-12-05 19:20:56 -0600

    complete

--- src/main/java/com/csc/OakTree.java
+++ src/main/java/com/csc/OakTree.java
@@ -1,7 +1,65 @@
 package com.csc;
 
 public class OakTree {
-  public static void main(String[] args) {
-    System.out.println("Oak Tree!!!");
-  }
+
+    public static class Squirrel {
+        private String name;
+
+        public Squirrel(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    public static class Node {
+        private Squirrel squirrel;
+        private Node left;
+        private Node right;
+
+        public Node(Squirrel squirrel) {
+            this.squirrel = squirrel;
+        }
+
+        public Squirrel getSquirrel() {
+            return squirrel;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Node left() {
+            return this.left;
+        }
+
+        public Node right() {
+            return this.right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Squirrel cheeks = new Squirrel("Cheeks");
+        Node nodeOne = new Node(cheeks);
+
+        Squirrel squeaks = new Squirrel("Squeaks");
+        Node nodeTwo = new Node(squeaks);
+
+        Squirrel fluffybutt = new Squirrel("Mr. Fluffy Butt");
+        Node nodeThree = new Node(fluffybutt);
+
+        nodeOne.setLeft(nodeTwo);
+        nodeOne.setRight(nodeThree);
+
+        System.out.println("Root Node: " + nodeOne.getSquirrel().getName());
+        System.out.println("Left Child: " + nodeOne.left().getSquirrel().getName());
+        System.out.println("Right Child: " + nodeOne.right().getSquirrel().getName());
+    }
 }
--- src/test/java/com/csc/TestOakTree.java
+++ src/test/java/com/csc/TestOakTree.java
@@ -1,21 +1,55 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestOakTree {
 
-  OakTree tree;
+    private OakTree.Node root;
+    private OakTree.Squirrel squirrel1;
+    private OakTree.Squirrel squirrel2;
+    private OakTree.Squirrel squirrel3;
 
-  @BeforeEach
-  void setUp() {
-    tree = new OakTree();
-  }
+    @BeforeEach
+    void setUp() {
+        squirrel1 = new OakTree.Squirrel("Cheeks");
+        squirrel2 = new OakTree.Squirrel("Squeaks");
+        squirrel3 = new OakTree.Squirrel("Mr. Fluffy Butt");
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+        root = new OakTree.Node(squirrel1);
+    }
+
+    @Test
+    void testAddChildren() {
+        OakTree.Node leftChild = new OakTree.Node(squirrel2);
+        OakTree.Node rightChild = new OakTree.Node(squirrel3);
+
+        root.setLeft(leftChild);
+        root.setRight(rightChild);
+
+        assertEquals("Squeaks", root.left().getSquirrel().getName());
+        assertEquals("Mr. Fluffy Butt", root.right().getSquirrel().getName());
+    }
+
+    @Test
+    void testInitialNode() {
+        assertEquals("Cheeks", root.getSquirrel().getName());
+        assertNull(root.left());
+        assertNull(root.right());
+    }
+
+    @Test
+    void testTreeTraversal() {
+        OakTree.Node leftChild = new OakTree.Node(squirrel2);
+        OakTree.Node rightChild = new OakTree.Node(squirrel3);
+
+        root.setLeft(leftChild);
+        root.setRight(rightChild);
+
+        assertEquals("Squeaks", root.left().getSquirrel().getName());
+        assertEquals("Mr. Fluffy Butt", root.right().getSquirrel().getName());
+    }
 }
